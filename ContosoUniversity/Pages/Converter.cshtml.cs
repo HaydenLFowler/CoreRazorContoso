8f99d67bb30fba30aaaf21bd39a475bf3fefcc7d
hayden fowler <hayd_4@live.co.uk>
2019-12-18 19:32:36 +0000

    Hello and Converter

---
+++ ContosoUniversity/Pages/Converter.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoUniversity.Pages
+{
+    public class ConverterModel : PageModel
+    {
+        public Decimal Pounds { get; set; }
+
+        public Decimal Euros { get; set; }
+
+        public CultureInfo France = new CultureInfo("fr-FR");
+
+        public string currencyFormat = "C2";
+
+        public void OnGet()
+        {
+            Pounds = 0m;
+            Euros = 0m;
+        }
+
+
+        public void OnPost()
+        {
+            string value = Request.Form["Pounds"];
+            Pounds = Convert.ToDecimal(value);
+
+            Euros = Pounds * 1.12m;
+        }        
+    }
+}
---
+++ ContosoUniversity/Pages/HelloWorld.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoUniversity.Pages
+{
+    public class HelloWorldModel : PageModel
+    {
+        
+            public string UserName { get; set; }
+
+            /// <summary>
+            /// This will be called when the web page first
+            /// loads.  It will set the name of the user to null
+            /// </summary>
+            public void OnGet()
+            {
+                UserName = null;
+            }
+            public void OnPost()
+            {
+                this.UserName = Request.Form["UserName"];
+            }
+        
+    }
+}
